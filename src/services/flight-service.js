19357a26a4b9043c8da12a0741591364e606dcf6
Adarsh <jha01adarsh@gmail.com>
2023-10-22 03:03:31 +0530

    Completed flight Service & REST operator

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -3,18 +3,51 @@ const { AirplaneRepository, FlightRepository } = require("../repository/index");
 class FlightService {
   constructor() {
     this.airplaneRepository = new AirplaneRepository();
+    this.flightRepository = new FlightRepository();
   }
 
   async createFlight(data) {
     try {
-      const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
-      return airplane;
+      const airplane = await this.airplaneRepository.getAirplane(
+        data.airplaneId
+      );
+      //   First we fetch the airplane & then add the capacity to flight
+      const flight = await this.flightRepository.createFlight({
+        ...data,
+        totalSeats: airplane.capacity,
+      });
+      return flight;
     } catch (error) {
       console.log("Something went wrong in service layer");
       throw { error };
     }
   }
+  async getFlightData(){
+    // TODO:
+  }
 }
+module.exports= FlightService;
+
+/**
+ * Explanation :
+ * await this.flightRepository.createFlight({
+        ...data,
+        totalSeats: airplane.capacity,
+      });
+
+-> In FlightRepository we have createFlight function, so data is the data that we fill in the form .
+* ...data , it means data object destructuring, means data = {a:10,b:20} we have this object. 
+ * if we do data= {...data, c:40}.
+ * our final object become {a:10,b:20,c:40}
+ * So, here we do ...data, spreaded it and add new parameter totalSeats , which derived from airplane.capacity(in airplane model we have capacity)
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ * /
+
 
 /** Data we get:
  * {
